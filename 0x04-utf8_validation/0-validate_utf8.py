4e4a29e32fb766f7eb061fb45ef0c90749386131
Thomasomoke <omokethomas644@gmail.com>
2024-10-31 08:00:46 +0000

    UTF-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,35 +1,39 @@
 #!/usr/bin/python3
 def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
+    # Bytes in current UTF-8 character
     num_bytes = 0
 
-    # Masks to check the first few bits of each byte
+    # Masks to check the first bits of each byte
     mask1 = 1 << 7   # 10000000
     mask2 = 1 << 6   # 01000000
 
     for byte in data:
-        # Only consider the 8 least significant bits of each integer
+        # Use only the 8 least significant bits
         byte = byte & 0xFF
 
         if num_bytes == 0:
-            # Determine how many bytes the current UTF-8 character has
+            # Determine UTF-8 byte length
             if (byte & mask1) == 0:
-                # 1-byte character (0xxxxxxx)
+                # 1-byte character
                 continue
             elif (byte & (mask1 | mask2)) == (mask1 | mask2):
-                # 2-byte character (110xxxxx)
+                # 2-byte character
                 num_bytes = 1
-            elif (byte & (mask1 | mask2 | (1 << 5))) == (mask1 | mask2 | (1 << 5)):
-                # 3-byte character (1110xxxx)
+            elif (byte & (mask1 | mask2 | (1 << 5))) == (
+                mask1 | mask2 | (1 << 5)
+            ):
+                # 3-byte character
                 num_bytes = 2
-            elif (byte & (mask1 | mask2 | (1 << 5) | (1 << 4))) == (mask1 | mask2 | (1 << 5) | (1 << 4)):
-                # 4-byte character (11110xxx)
+            elif (byte & (mask1 | mask2 | (1 << 5) | (1 << 4))) == (
+                mask1 | mask2 | (1 << 5) | (1 << 4)
+            ):
+                # 4-byte character
                 num_bytes = 3
             else:
-                # Invalid UTF-8 byte
+                # Invalid byte
                 return False
         else:
-            # The following bytes must be 10xxxxxx
+            # Following bytes must be 10xxxxxx
             if (byte & (mask1 | mask2)) != mask1:
                 return False
             num_bytes -= 1

61f5753b67fbdf4fae6ee7d91e77ba829600bd8c
Thomasomoke <omokethomas644@gmail.com>
2024-11-27 13:25:05 +0000

    new

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+"""
+Determine the fewest number of coins needed to meet a total.
+"""
+
+
 def makeChange(coins, total):
+    """
+    Returns the fewest number of coins to make the total
+    """
     if total <= 0:
         return 0
 
-    # Initialize the DP table with a value larger than any possible coin count
     dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: 0 coins are needed to make 0 total
+    dp[0] = 0
 
-    # Fill the DP table
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    # If dp[total] is still infinity, then it is not possible to make the total
     return dp[total] if dp[total] != float('inf') else -1
